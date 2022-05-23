c3c440efe247c8a6efdd12bd053560d97e0be40f
Blossom <saphashb@gmail.com>
2022-05-23 10:42:26 +0100

    create db environment

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
-import express, { Request, Response, Application } from 'express'
+import express, { Request, Response, Application } from "express";
 
-const app: Application = express()
-const port = 8050
+const app: Application = express();
+const port = 8050;
 
-app.get('/', (req: Request, res: Response) => {
-  res.status(200).json({ Message: 'Welcome to Zion School Management system Route' })
-})
+app.get("/", (req: Request, res: Response) => {
+  res
+    .status(200)
+    .json({ Message: "Welcome to Zion School Management system Route" });
+});
 
-app.get('*', (req: Request, res: Response) => {
-  res.status(200).json({ Message: 'You have accessed a route that does not exist.' })
-})
+app.get("*", (req: Request, res: Response) => {
+  res
+    .status(200)
+    .json({ Message: "You have accessed a route that does not exist." });
+});
 
-app.listen(port)
+app.listen(port);
